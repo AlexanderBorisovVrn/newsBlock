5be861a87f63ebadc23d0e3be7ac103c4b5b094a
AlexanderBorisovVrn <sanchosdisaster@mail.ru>
2021-05-06 19:10:23 +0300

    feat:add sign-in/sign-up button show

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './header.scss';
+import Profile from '../sing-buttons';
 import {Link} from 'react-router-dom';
 import img from './logo.png';
 
@@ -36,27 +37,7 @@ const Header = () => {
             )
           })}
         </nav>
-        <button className='profile'>
-          <div className='profile__wrapper'>
-            <div className='profile__name'>
-              <span>василь</span>
-            </div>
-            <div className='profile__img-wrap'>
-              <img
-                alt=''
-                className='profile__img'
-                src='https://images.unsplash.com/photo-1604537529428-15bcbeecfe4d?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=749&q=80'/>
-            </div>
-          </div>
-          <div className={profileInHover}>
-            <button type='button' className='profile__sign-up'>
-              <span>Регистрация</span>
-            </button>
-            <button type='button' className='profile__sign-in'>
-              <span>войти</span>
-            </button>
-          </div>
-        </button>
+        <Profile/>
       </div>
     </header>
   )
