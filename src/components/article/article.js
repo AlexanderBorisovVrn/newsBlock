fa4fff6a780bcc8e4e946e4da7dd4364fe297eaa
AlexanderBorisovVrn <sanchosdisaster@mail.ru>
2021-11-20 16:22:18 +0300

    feat:add comments for article

--- src/components/article/article.js
+++ src/components/article/article.js
@@ -1,35 +1,38 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './article.scss';
-import Comments from '../comments/comments';
-
+import CommentsContainer from '../containers/comments-container/commentsComtainer'
 const Article = ({children}) => {
-  const {title, urlToImage,content,author} = children;
-  //принимает из article-container данные
-  //отображает статью
-  
+  const {title, urlToImage, content, author} = children;
+  //принимает из article-container данные отображает статью
+
   return (
     <article className='article'>
-        <div className='article__inner'>
+      <div className='article__inner'>
         <h1 className='article__title'>{title}</h1>
         <p className='article__author'>{author}</p>
-          <div className='article__img-wrap'>
-            <img className='article__img' src={urlToImage} alt='' width='1200' height='500'/>
-          </div>
-          <div className='article__txt'>
-            <p>{content}</p>
-          </div>
-          <Comments/>
+        <div className='article__img-wrap'>
+          <img
+            className='article__img'
+            src={urlToImage}
+            alt=''
+            width='1200'
+            height='500'/>
+        </div>
+        <div className='article__txt'>
+          <p>{content}</p>
+        </div>
+        <CommentsContainer/>
       </div>
     </article>
   )
 }
 
-Article.propTypes={
-  title:PropTypes.string,
-  description:PropTypes.string,
-  urlToImage:PropTypes.string,
-  id:PropTypes.number
+Article.propTypes = {
+  title: PropTypes.string,
+  description: PropTypes.string,
+  urlToImage: PropTypes.string,
+  id: PropTypes.number
 }
 
 export default Article;
---
+++ src/components/containers/comments-container/commentsComtainer.js
@@ -0,0 +1,89 @@
+import React, {useRef, useState} from 'react';
+import {
+  comments,
+  inner,
+  header,
+  title,
+  toggleOff,
+  toggleOn,
+  tabs,
+  tabsList,
+  item,
+  text
+} from './commentsContainer.module.scss'
+
+const com = [
+  {
+    author: 'dim mednet',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sed sollicitudin' +
+        ' dolor. Nulla volutpat, metus et varius luctus, orci risus tempus purus, ut vari' +
+        'us dui velit aliquet tortor. Duis volutpat, dolor quis pulvinar blandit, orci er' +
+        'at facilisis urna, a egestas augue ante in felis. Ut quis fermentum risus, ac vu' +
+        'lputate quam. Aenean volutpat arcu in convallis sollicitudin. Ut nec eros posuer' +
+        'e, eleifend ex sed, blandit justo. Quisque metus velit, semper sed semper eu, eg' +
+        'estas sed orci. Quisque in vestibulum lectus, efficitur varius ipsum. Quisque po' +
+        'suere pulvinar metus, et auctor enim porta sed. Proin quis tincidunt sem. Quisqu' +
+        'e ultricies quam ac.',
+    dateTime: '2019-08-21'
+  }, {
+    author: 'trew stratway',
+    text: 'Maecenas tristique scelerisque malesuada. Nunc lacinia pretium risus, ut pretium' +
+        ' mauris hendrerit quis. Duis eu lorem laoreet, tincidunt ligula at, volutpat mas' +
+        'sa. Vestibulum consequat nisl at mi tempus tristique. Nunc eu lectus rhoncus, ve' +
+        'stibulum diam eu, sagittis mi. Aliquam vel quam quis sapien maximus condimentum ' +
+        'vitae non nisl. Praesent eget massa ut neque sodales ornare a facilisis nisl. Ma' +
+        'uris sit amet gravida lectus. Etiam a erat est. Donec eget faucibus urna, sit am' +
+        'et condimentum nunc. Nulla accumsan tellus mauris, id convallis mauris ultricies' +
+        ' eu. Nulla eget urna sit amet massa rhoncus fermentum sit amet id nisl. Curabitu' +
+        'r congue libero eu justo pulvinar ultricies. Ut ipsum augue, rhoncus a nulla in,' +
+        ' interdum fringilla leo. Nunc nec diam et lectus porttitor iaculis. Phasellus in' +
+        ' dui risus.',
+        dateTime:'2019-08-22'
+  }
+]
+
+const CommentsContainer = () => {
+  const [isShowComments,
+    setIsShowComments] = useState(false);
+    const [textareaHeight,
+      setTextareaHeight] = useState(80);
+  const ref = useRef();
+  return (
+    <section className={comments}>
+      <div className={inner}>
+        <div className={header}>
+          <h2 className={title}>Комментарии:<span>&nbsp;1</span>
+          </h2>
+          <button
+            type='button'
+            className={isShowComments
+            ? toggleOff
+            : toggleOn}
+            onClick={() => setIsShowComments(!isShowComments)}></button>
+        </div>
+        <div className={tabs}>
+          <ul className={tabsList}>
+            <li className={item}>Новые</li>
+            <li className={item}>Старые</li>
+            <li className={item}>Популярные</li>
+          </ul>
+        </div>
+        <textarea
+          style={{
+          height: textareaHeight + 'px'
+        }}
+          ref={ref}
+          onChange={() => {
+         if(ref.current.scrollTop>0){
+           setTextareaHeight(textareaHeight+ref.current.scrollTop)
+         }
+        }}
+          wrap='soft'
+          className={text}
+          placeholder='Напишите комментарий...'></textarea>
+      </div>
+    </section>
+  );
+}
+
+export default CommentsContainer;
